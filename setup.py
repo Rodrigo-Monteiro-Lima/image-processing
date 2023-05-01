0b8df3ef8d382ba3bc2e39971af947ac7c45a2ae
Rodrigo <rodrigoml2395@gmail.com>
2023-05-01 07:30:25 -0300

    fix: rename

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setup(
-    name="image_processing",
-    version="0.0.2",
+    name="img_processing_test",
+    version="0.0.1",
     author="Rodrigo Monteiro Lima",
     author_email="rodrigoml2395@gmail.com",
     description="Test version - Image processing. This project belongs to Karina Tiemi Kato, Tech Lead, Machine Learning Engineer, Data Scientist Specialist at Take. This package is a demo for simulation of upload on the Test Pypi website, and it's from class of the Bootcamp developer full stack Python. E-mail:karinatkato@gmail.com.",
